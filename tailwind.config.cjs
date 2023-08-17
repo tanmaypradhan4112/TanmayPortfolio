67da1bc45cce7ebb962bdee376c2025936a56e6d
tanmaypradhan4112 <tanmaypradhan4112@gmail.com>
2023-08-17 16:31:06 +0530

    Revamped the website

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				preigotext : ["PreigoText"],
+				preigohead : ["PreigoHeading"]
+			  },
+		},
 	},
 	plugins: [require("@tailwindcss/typography"),require("daisyui")],
 	daisyui: {
